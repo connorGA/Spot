2827d0bdaa059afa8ccea8f763b6c04182a35422
connorGA <ronnoccap@gmail.com>
2023-09-01 22:30:47 -0700

    added search page basic views and url, updated settings.py with core app

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Home")
 
+def search(request):
+    query = None
+    if 'q' in request.GET:
+        query = request.GET['q']
+    return render(request, 'search.html', {'query': query})
