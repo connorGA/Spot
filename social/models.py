973e76f00a860e8fde429e04de07187b0aa9f095
connorGA <ronnoccap@gmail.com>
2024-09-18 15:14:06 -0700

    add UserProfile model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    spotify_id = models.CharField(max_length=255)
+    profile_picture = models.URLField(null=True, blank=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
+
+    def __str__(self):
+        return self.user.username
 
 # Create your models here.
